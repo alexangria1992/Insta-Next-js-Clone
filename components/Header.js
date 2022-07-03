2b28b05fdb782aa59937dc22e46bd1e391e3aef6
alexangria1992 <alex040892@gmail.com>
2022-07-03 19:16:12 +1200

    Middle Section of Header

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Image from "next/image";
-
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
 function Header() {
   return (
     <div>
       <div className="flex justify-between max-w-6xl">
         {/* Left */}
-        <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+        <div className="relative hidden lg:inline-grid  w-24 cursor-pointer">
           <Image
             src="https://links.papareact.com/ocw"
             layout="fill"
@@ -14,7 +21,7 @@ function Header() {
           />
         </div>
 
-        <div className="relative h-10 w-10 lg:hidden flex-shrink-0 cursor-pointer">
+        <div className="relative w-10  lg:hidden flex-shrink-0 cursor-pointer">
           <Image
             src="https://links.papareact.com/jjm"
             layout="fill"
@@ -23,8 +30,20 @@ function Header() {
         </div>
 
         {/* Middle */}
+        <div className="max-w-xs">
+          <div className="mt-1 relative p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black "
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
 
-        {/* Right */}
+        {/* RIGHT */}
       </div>
     </div>
   );
